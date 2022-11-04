cf4b4238b767e617545fbb0020d85348d301dd9c
HDR628 <henriquedias628@gmail.com>
2022-11-04 12:12:15 -0300

    revisao do exercicio 4

--- Javascript_Modulo_1/Exercicio-4/traveler.js
+++ Javascript_Modulo_1/Exercicio-4/traveler.js
@@ -1,15 +1,15 @@
 const name = prompt("Qual seu nome?");
 let question = prompt(name + " voce ja visitou alguma cidade? (sim/não)");
 let numberCity = 0;
-let allCitys;
+let allCitys = " ";
 
 while (question !== "não") {
   let city = prompt("Serio :) qual cidade?");
   allCitys += city + ", ";
   numberCity++;
-  question = prompt("Voce ja visitou mais alguma ? (não/não)");
+  question = prompt("Voce ja visitou mais alguma ? (sim/não)");
 }
-/* allCitys it's showing undefined before input from users */
+/* A saida undefined foi resolvida apenas adicionando/iniciando a variavel allCitys com " empty "   */
 alert(
   name +
     " ja visitou um total de " +
