2bca53ae97906742e40b4dafdb6eba27d420e26f
Henrique Dias Ribeiro <henriquedias628@gmail.com>
2023-04-15 15:16:48 -0300

    adicionar aula/anotações sobre  eventos

---
+++ Javascript_Modulo_2/Aulas/Eventos/eventos.js
@@ -0,0 +1,11 @@
+function onClick() {
+  console.log(`Evento onClick ocorreu`)
+}
+
+function onChange() {
+  console.log("Mudou elemento selecionado")
+}
+
+function onFocus() {
+  console.log("Ae focou")
+}
