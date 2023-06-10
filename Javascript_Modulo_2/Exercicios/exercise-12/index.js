23d1b744aa13b212deb29f7456265af7a97b0a9a
Henrique Dias Ribeiro <henriquedias628@gmail.com>
2023-06-10 14:59:16 -0300

    create function input and label

--- Javascript_Modulo_2/Exercicios/exercise-12/index.js
+++ Javascript_Modulo_2/Exercicios/exercise-12/index.js
@@ -1,9 +1,26 @@
+function createLabel(text, htmlFor) {
+  const label = document.createElement(`label`)
+  label.htmlFor = htmlFor
+  label.innerText = text
+  return label
+}
+
+function createInput(id, value, name, type = `text`, placeholder = ``) {
+  const input = document.createElement(`input`)
+  input.id = id
+  input.value = value
+  input.name = name
+  input.type = type
+  input.placeholder = placeholder
+  return input
+}
+
 let addTechButton = document.getElementById(`addTechButton`)
 
 addTechButton.addEventListener(`click`, function (ev) {
   ev.preventDefault()
 
-  let localReference = document.getElementById(`formHere`)
+  let localReference = document.getElementById(`formDev`)
 
   let div = document.createElement(`div`)
   div.id = Math.floor(Math.random() * 50) + 1
