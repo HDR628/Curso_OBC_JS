876e7ddfb02d510904893b88b4a46714139ba63b
Henrique Dias Ribeiro <henriquedias628@gmail.com>
2023-06-27 16:56:31 -0300

    feat: add test for turns and images

--- Javascript_Modulo_2/Exercicios/exercise-13/script.js
+++ Javascript_Modulo_2/Exercicios/exercise-13/script.js
@@ -2,4 +2,14 @@
 [ ] Como saber o turno de cada jogador ?
 [ ] Como capturar o elemento/espaço a ser usado
 [ ] Como passar/criar a imagem a partir do clique do usuário
+[ ] Definir turno ?
 */
+
+document.getElementById(`space1`).addEventListener(`click`, function () {
+  if (gridArea.dataset.turn === `playerOneTurn`) {
+    alert("Jogador 1")
+    space1.classList = "xTurn"
+  } else {
+    alert("Jogador 2")
+  }
+})
