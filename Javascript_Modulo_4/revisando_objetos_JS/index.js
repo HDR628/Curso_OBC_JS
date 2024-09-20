8dcdba3d420d3857da49b7409104a0561ea60c71
Henrique <henriquedias628@gmail.com>
2024-09-20 17:07:06 -0300

    POO - Inicio

---
+++ Javascript_Modulo_4/revisando_objetos_JS/index.js
@@ -0,0 +1,29 @@
+/* let object = {
+    key: value
+} */
+    function Book(title, pages, tags, author) {
+        this.title = title //referencia ao parametro 
+        this.pages = pages
+        this.tags = tags
+        this.author = author
+        this.published = false
+        this.inStock = 0
+        this.addOnStock = function addOnStock(quantity) {
+          this.inStock += quantity
+        }
+        this.save = function () {
+          // Save book to database
+        }
+      }
+      
+      const author = { name: "Christopher Paolini" } // para uso posterior/geral
+      const tags = ["fantasy", "adventure", "medieval"] 
+      
+      const eragon = new Book("Eragon", 468, tags, author)
+      
+      eragon.addOnStock(20) 
+      
+      const eldest = new Book("Eldest", 644, tags, author)
+      
+      console.log(eragon)
+      console.log(eldest)
