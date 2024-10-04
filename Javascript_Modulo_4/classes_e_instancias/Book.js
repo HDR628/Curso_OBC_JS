2bd9744c12a90b85c631f356596a8c7528116811
Henrique <henriquedias628@gmail.com>
2024-10-04 16:51:25 -0300

    aula sobre classes/instancias

---
+++ Javascript_Modulo_4/classes_e_instancias/Book.js
@@ -0,0 +1,22 @@
+class Book  {
+    constructor(title){  ///atributos - dados
+        this.title = title
+        this.published = false
+    }
+    //metodos
+    publish(){
+        this.published = true
+    }
+
+}
+// eragon = instancia e book = classe
+const eragon = new Book("Eragon")
+const toradora = new Book("Tiger")
+
+// criando metodo published
+toradora.publish()
+
+console.log(eragon)
+console.log(toradora)
+
+console.log(toradora instanceof Book) //confirma se a instancia e do objeto/classe, no caso Book
