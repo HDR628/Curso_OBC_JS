13e1f244ae4a9909b5855d47d6316fb14d0a6f95
Henrique <henriquedias628@gmail.com>
2025-01-12 19:25:36 -0300

    feat: add Class Mage

---
+++ Javascript_Modulo_4/exercise_21/Mage.js
@@ -0,0 +1,25 @@
+import {Character} from "./Character"
+
+class Mage extends Character{
+    constructor(name,lifePoints,attack,magePoints,defense){
+        super(name,lifePoints,attack,defense)
+        this.magePoints = magePoints
+    }
+
+    targetCharacter(target){
+        if(this.attack > target.defense){
+            let damageLevel = (this.attack + this.magePoints) - target.defense
+            target.lifePoints =  target.lifePoints - damageLevel
+        }else{
+            return `O alvo não perdeu de pontos de vida, por sua defesa ser maior`
+        }
+    }
+
+    healingCharacter(aimHeal){
+        let defaultHealing = this.magePoints * 2
+        let healTotal = aimHeal.lifePoints + defaultHealing
+        aimHeal.lifePoints = healTotal
+
+        console.log(`O personagem ${aimHeal.name} foi curado pelo mago, restaurando ${healTotal} de sua vida`)
+    }
+}
