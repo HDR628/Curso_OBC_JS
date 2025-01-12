cde8735d72e21a51d1beddc4dfbef918f2ca9fc3
Henrique <henriquedias628@gmail.com>
2025-01-12 18:57:42 -0300

    feat: inicio exercicio 21

---
+++ Javascript_Modulo_4/exercise_21/Character.js
@@ -0,0 +1,27 @@
+class Character {
+    constructor(name,lifePoints, attack, defense){
+        this.name = name
+        this.lifePoints = lifePoints
+        this.attack = attack
+        this.defense = defense
+    }
+
+    /* Personagem alvo  */
+    targetCharacter(target){
+        if(this.attack > target.defense){
+            let damageLevel = this.attack - target.defense
+            target.lifePoints =  target.lifePoints - damageLevel
+        }else{
+            return `O alvo não perdeu de pontos de vida, por sua defesa ser maior`
+        }
+    }
+}
+/* const one = new Character("H",100,70,60)
+const two = new Character("J",100,30,60)
+console.log(one)
+console.log(two)
+
+one.targetCharacter(two)
+console.log(two) */
+
+
