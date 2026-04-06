48ca7493efd3e19a427351602dac4412c5b09631
Henrique <henriquedias628@gmail.com>
2026-04-05 21:12:57 -0300

    Promise.all

---
+++ Javascript_Modulo_5_Final/Executando várias Promises/index.js
@@ -0,0 +1,49 @@
+function asyncSum(a, b) {
+  return new Promise((resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject("arguments must be of type number");
+    } else {
+      resolve(a + b);
+    }
+  });
+}
+
+function asyncSubtract(a, b) {
+  return new Promise((resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject("arguments must be of type number");
+    } else {
+      resolve(a - b);
+    }
+  });
+}
+
+const sumResult = asyncSum(50, 33);
+const subtractResult = asyncSubtract(50, 10);
+// const subtractResult = asyncSubtract(50, null); Com esse parametros da erro
+
+Promise.all([sumResult, subtractResult])
+  .then((results) => {
+    console.log(results);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+console.log("Divisão do terminal, outro exemplo de uso da promisse all");
+
+const numbers = [4, 9, 5, 13, 77]; /* Criação de um array de numeros */
+
+/* Obter o quadrado de cada número */
+function asyncSquare(x) {
+  return new Promise((resolve, reject) => {
+    resolve(x * x);
+  });
+}
+
+/* O map nos retorna um array.
+Promise.all() retorna um array de promises caso todas sejam resolvidas, retorna um array com os resultados respectivamente */
+/* Tambem podemos utilizar os tratamentos ja usados como then,catch e finally. Encadeando/concatenando o metodo map neles */
+Promise.all(numbers.map((number) => asyncSquare(number))).then((squares) => {
+  console.log(squares);
+});
