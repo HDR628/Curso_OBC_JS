48fb2f00daac5bba2254e4c34a9a4e0065635ecb
Henrique <henriquedias628@gmail.com>
2026-04-17 12:43:09 -0300

    fix: make function logic work

--- exercise_create_types_ts/src/index.ts
+++ exercise_create_types_ts/src/index.ts
@@ -3,7 +3,6 @@
 let allPlanets: Planet[] = []; //Pra listar todos depois
 
 type situationPlanet = "habitado" | "habitavel" | "inabitavel" | "inexplorado";
-
 type cordenadas = [number, number, number, number];
 
 interface Planet {
@@ -13,86 +12,141 @@ interface Planet {
   satelites: string[];
 }
 
-function getSituationPlanet(situation: situationPlanet){
+function createPlanet(){
+  let name = String(prompt("Qual o nome do planeta ?") || "") // Caso o usuario nao digite nada
 
-  const validSituations: situationPlanet[] = ["habitado", "habitavel", "inabitavel", "inexplorado"];
+  while(name === ""){
+    alert("Nome invalido, digite novamente...")
+    name = String(prompt("Qual o nome do planeta ?") || "")
+  }
 
-  while(true){
-    let newSituation = String(prompt("Qual a nova situação do planeta ?"))
+ /* Como estava recebendo 1 texto de numeros, creio que resolve se eu pegar 1 por 1 */
+  let cord1 = Number(prompt("Qual a primeira cordenada ?"))
+  let cord2 = Number(prompt("Qual a segunda cordenada ?"))
+  let cord3 = Number(prompt("Qual a terceira cordenada ?"))
+  let cord4 = Number(prompt("Qual a quarta cordenada ?"))
 
-    if (newSituation !== "habitado" && newSituation !== "habitavel" && newSituation !== "inabitavel" && newSituation !== "inexplorado"){
-      alert("Situação invalida, escolha entre habitado, habitavel, inabitavel ou inexplorado...")
-      return;
+  let cordenadas: cordenadas = [cord1, cord2, cord3, cord4] // Se passar texto da ruim
+  
+  // 1 vez usando o metodo some, pelo que entendi e como se perguntase "tem alguem aqui que atende essa condição?" ele retorna true/false se 1 qualquer cordenada for do tipo NaN ele retorna true...
+  while(cordenadas.some((valor) => Number.isNaN(valor))){
+    alert("Cordenadas invalidas, digite novamente...")
+    cord1 = Number(prompt("Qual a primeira cordenada ?"))
+    cord2 = Number(prompt("Qual a segunda cordenada ?"))
+    cord3 = Number(prompt("Qual a terceira cordenada ?"))
+    cord4 = Number(prompt("Qual a quarta cordenada ?"))
+
+    cordenadas = [cord1, cord2, cord3, cord4];
+  }
+
+  // if(cordenadas.includes(NaN) || ""){
+  //   alert("Cordenadas invalidas, digite novamente...")
+  //   cord1 = Number(prompt("Qual a primeira cordenada ?"))
+  //   cord2 = Number(prompt("Qual a segunda cordenada ?"))
+  //   cord3 = Number(prompt("Qual a terceira cordenada ?"))
+  //   cord4 = Number(prompt("Qual a quarta cordenada ?"))
+  // } So funcionava a 1 verificaçào, caso passase 1 tipo diferente nos demais tentativas ele passava como "numero"
+  // TODO[X]: Verificar se da erro ao passar um texto ou outro caractere, ai coloco igual ali em cima o while  
+ 
+
+  let situation: situationPlanet
+  while(true){ // devido ao true inicia 1 vez 
+    const situationInput = String(prompt("Qual a situação do planeta ? (habitado, habitavel, inabitavel, inexplorado)"))
+    // let onlyAccept = ["habitado","habitavel","inabitavel","inexplorado"]
+    if(["habitado", "habitavel", "inabitavel", "inexplorado"].includes(situationInput)){ /// se o input do usuario for um desse e estiver incluido no situationInput ele entra no if
+      situation = situationInput as situationPlanet;
+      break;
     }else{
-      return newSituation as situationPlanet;
+      alert("Situação invalida, tente novamente")
+    }
+  }
+
+  const planet: Planet = {
+    name,
+    cordenadas,
+    situation,
+    satelites: [] 
+  }
+
+  allPlanets.push(planet)
+  return planet
+}
+
+
+function findPlanet(name: string) { // Vi no site do mdn, estava com dificuldade de lembrar como usar "certinho"
+  return allPlanets.find((nomePlaneta) => nomePlaneta.name === name  )
+} 
+
+function updatePlanet(name: string,newSituation?: string){
+  let planetName = findPlanet(name)
+
+  if(!planetName){
+    alert("Esse planeta não esta registrado")
+    return
+  }else {
+    newSituation = String(prompt(`Qual a nova situação do planete ?\n
+      habitado\n
+      habitavel\n
+      inabitavel\n
+      inexplorado\n `))
+
+      if(newSituation !== "habitado" && newSituation !==  "habitavel" && newSituation !== "inabitavel" && newSituation !== "inexplorado"){
+        alert("informe um valor valido")
+        newSituation = String(prompt(`Qual a nova situação do planete ?\n
+          habitado\n
+          habitavel\n
+          inabitavel\n
+          inexplorado\n `))
+      }else{
+        planetName.situation = newSituation
+      }
+  }
+}
+
+
+function addSatelite(name:string,sateliteName?: string){
+  let planetName = findPlanet(name)
+
+  if(!planetName){
+    alert("Esse planeta não existe!")
+  }else{
+    sateliteName = String(prompt(`Informe o nome do novo satelite: `))
+    if(planetName.satelites.includes(sateliteName)){
+      alert("Esse planeta ja tem esse satelite, não e possivel adicionar!")
+    }else{
+      planetName.satelites.push(sateliteName)
+      alert("Satelite add com sucesso!")
     }
   }
 }
 
+function removeSatelite(name: string, sateliteName?: string){
+  let planetName = findPlanet(name)
 
-function createPlanet(name: Planet, cordenadas: Planet, sitaution: situationPlanet) {
-  const planet = {
-    name: String(prompt("Qual o nome do planeta ?")),
-    cordenadas: Number(prompt("Cordenadas do planeta ? ")),   // Estou recebendo basicamente 1 texto de numeros assim : 1234
-    situation: String(prompt("Qual a situação do planeta ?")),
-    satelites: [] as string[], // esse as serve pra tipo falar isso aqui vai ser um array de strings, para nao dar erro de tipagem
-  };
-
-  // allPlanets.push(planet as Planet);
-  return planet;
-}
-
-
-
-function updatePlanet(planetName: String, situation: string){
-
-  let findPlanet = allPlanets.find((planet: Planet) => planet.name === planetName)
-
-  if(findPlanet){
-    let newSituation = String(prompt("Qual a nova situação do planeta ?"))
-
-    if (newSituation !== "habitado" && newSituation !== "habitavel" && newSituation !== "inabitavel" && newSituation !== "inexplorado"){
-      alert("Situação invalida, escolha entre habitado, habitavel, inabitavel ou inexplorado...")
-      return;
-    }else{
-      findPlanet.situation = newSituation as "habitado" | "habitavel" | "inabitavel" | "inexplorado"
-      return findPlanet.situation as "habitado" | "habitavel" | "inabitavel" | "inexplorado";
-    }
-  } else {
-    alert("Planeta nao encontrado...")
-  }
-
-  alert("Planeta atualizado com sucesso!")
-}
-
-
-function addSatelite(planetName: String, sateliteName: String){
-
-  let findPlanet = allPlanets.find((planet: Planet) => planet.name === planetName) // Da pra fazer uma função pra não ter que ficar repetindo 
-
-  if(findPlanet){
-    findPlanet.satelites.push(sateliteName as string)
+  if(!planetName){
+    alert("Esse planeta não existe")
   }else{
-    alert("Planeta nao encontrado...")
+    alert(`Atualmente temos esses satelites ao entorno desse planeta: \n
+      ${planetName.satelites}`)
+    sateliteName = String(prompt("Qual satelite deseja remover ?"))
+    let sateliteIndex = planetName.satelites.indexOf(sateliteName)
+
+    planetName.satelites.splice(sateliteIndex,1)
+    alert("Removido com sucesso")
   }
 }
-
-
-function removeSatelite(planetName: String, sateliteName: String){
-
-  let findPlanet = allPlanets.find((planet: Planet) => planet.name === planetName)
-
-  if(findPlanet){
-    if(findPlanet.satelites.includes(sateliteName as string)){
-    findPlanet.satelites.splice(findPlanet.satelites.indexOf(sateliteName as string), 1)
-    alert("Satelite removido com sucesso!")
-  }else{
-    alert("Satelite nao encontrado...")
-  }
-
-}}
-
 
 function listPlanets(){
-  return console.log(allPlanets);
+  console.log(allPlanets)
 }
+
+
+
+createPlanet()
+listPlanets()
+updatePlanet("Marte")
+listPlanets()
+addSatelite("Marte")
+listPlanets()
+removeSatelite("Marte")
+listPlanets()
