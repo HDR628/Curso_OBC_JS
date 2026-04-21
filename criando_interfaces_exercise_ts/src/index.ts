319e24ad86de68e06a66574d27e8d226c36902a5
Henrique <henriquedias628@gmail.com>
2026-04-21 16:27:52 -0300

    add: exercise of interfaces with ts complete - waiting for correction

--- criando_interfaces_exercise_ts/src/index.ts
+++ criando_interfaces_exercise_ts/src/index.ts
@@ -45,16 +45,70 @@ async function showRepos(login: string){
     if(!exist){
         console.log("Usuario não encontrado")
     }else{
-        let response = await fetch(`https://api.github.com/users/${login}/repos`)
+        // let response = await fetch(`https://api.github.com/users/${login}/repos`)
+        let response = await fetch (exist.repos_url)
         let arrRepos : GithubRepoResponse[] = await response.json()
         console.log(arrRepos)
     }
 }
 
 
-/* async function main(){
-    await  getUsernameLogin("hdr628")
-    await showRepos("hdr628")
+function listUsers(){
+    if(allUsers.length === 0){
+        console.log("Nenhum usuario salvo.")
+        return
+    }
+
+    console.log("Usuarios salvos:")
+    allUsers.forEach((user, index) => {
+        console.log(`${index + 1}. ${user.login} (${user.name}) - ${user.public_repos} repos`)
+    })
 }
 
-main() */
+
+function calcRepos(){
+    let total = 0 
+
+    allUsers.forEach((user) =>
+        total+= user.public_repos
+    )
+
+    console.log(total)
+} 
+
+
+function topFiveUser(){
+    if(allUsers === undefined || allUsers.length < 1 ){
+        console.log("Usuarios insuficientes...")
+        return
+    }else{
+        let copyUser = [...allUsers] // copia do arr pra nao alterar o original
+        let sorterCopy = copyUser.sort((a,b) => b.public_repos - a.public_repos).slice(0,5) // descendente e pega os 5 primeiros com mais repos publico
+        console.log(sorterCopy)
+    
+        // exibição como pede o exercicio
+        sorterCopy.forEach((user, index) => {
+            console.log(`${index + 1}. ${user.login} - ${user.public_repos} repos`)
+        })
+
+        // let sortedArr = allUsers.sort((repoB,repoA) => repoB.public_repos - repoA.public_repos)
+        // let topFive = sortedArr
+        // let sliceFive = (topFive.slice(0,5))    
+
+        // console.log(sliceFive)
+    }
+}
+
+
+async function main() {
+    await getUsernameLogin("hdr628")
+    await getUsernameLogin("octocat")
+    await getUsernameLogin("torvalds")
+  
+    listUsers()
+    calcRepos()
+    topFiveUser()
+    await showRepos("hdr628")
+  }
+  
+  main()
