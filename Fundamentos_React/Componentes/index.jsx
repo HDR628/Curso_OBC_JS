d5e66c998065914b5702f801d410e46ef11b4604
Henrique <henriquedias628@gmail.com>
2026-04-23 15:45:35 -0300

    Componentes notes

---
+++ Fundamentos_React/Componentes/index.jsx
@@ -0,0 +1,12 @@
+function Button() {
+  return <button>Botão teste</button>;
+}
+
+function Container() {
+  return (
+    <div classname="Classe">
+      <h1>H1</h1>
+      <p>Opa</p>
+    </div>
+  );
+}
