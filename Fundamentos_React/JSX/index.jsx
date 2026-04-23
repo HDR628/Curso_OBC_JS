6f6d2e9e2fc550d8ea36d093f919d363f022dde2
Henrique <henriquedias628@gmail.com>
2026-04-23 12:23:38 -0300

    Add jsx file/examples

---
+++ Fundamentos_React/JSX/index.jsx
@@ -0,0 +1,12 @@
+const element = <h1>Hello, world</h1>;
+
+const anotherElement = (
+  <div>
+    <h1>Opa</h1>
+    <ul>
+      <li>One</li>
+      <li>Two</li>
+    </ul>
+    <h2>Nice</h2>
+  </div>
+);
