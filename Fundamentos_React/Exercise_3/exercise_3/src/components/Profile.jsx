afaf28603be07f4f40d3589617a2480be21e86ff
Henrique <henriquedias628@gmail.com>
2026-04-27 12:20:53 -0300

    feat: add function handleFollow, learn events

--- Fundamentos_React/Exercise_3/exercise_3/src/components/Profile.jsx
+++ Fundamentos_React/Exercise_3/exercise_3/src/components/Profile.jsx
@@ -3,14 +3,25 @@ import { Button } from "./Button/Button"
 import { ProfileSection } from "./ProfileSection/ProfileSection"
 import { Title } from "./Title"
 
+/* Por padrão nos declaramos as funções antes do return caso seja necessario acessar alguma props, mas caso seja algo simples como nesse caso poderiamos fazer de forma
+inline mesmo.Tambem podemos declarar ela um arquivo separado e depois importá-la 
+*/
+function handleFollow(ev) {
+  console.log(ev)
+  alert("Voce agr esta seguindo")
+}
 export function Profile({avatar, bio, email, phone, githubUrl, children}){
+
+
   return (
     <div className={styles.profile}>
       <img src={avatar} alt={"Imagem de Perfil"} />
       <Title>
         <p>Nome</p>
         {children}
-        <button>Follow</button>
+        <button onClick={handleFollow}> 
+          Follow
+        </button>
       </Title>
       <ProfileSection>
         <hr />
