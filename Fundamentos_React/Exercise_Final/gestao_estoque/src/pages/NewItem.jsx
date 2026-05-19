8a5205c100bddb272ab4360ee3010d621243000e
Henrique <henriquedias628@gmail.com>
2026-05-19 13:04:32 -0300

    feat: abas de navegação e encapsulamento com o form

--- Fundamentos_React/Exercise_Final/gestao_estoque/src/pages/NewItem.jsx
+++ Fundamentos_React/Exercise_Final/gestao_estoque/src/pages/NewItem.jsx
@@ -1,21 +1,47 @@
+import { Link } from "react-router-dom"
+
 export function NewItem(){
   return(
     <>
-      <label htmlFor="itemName">Nome:</label>
-      <input type="text" id="itemName" /> 
-      <label htmlFor="itemQtd">Quantidade</label>
-      <input type="number" id="itemQtd"/>
-      <label htmlFor="priceItem">Preço</label>
-      <input type="number" id="priceItem" />
-      <label htmlFor="category">Categoria</label>
-      <select name="category" id="category">
-        <option value="OptionA">Option A</option>
-        <option value="OptionB">Option B</option>
-      </select> {/* Substituir as opçoes posteriormente */}
-      <label htmlFor="description">Descrição</label>
-      <textarea name="description" id="description">
-      </textarea>
-      <button type="submit">Salvar</button>
+
+      <div className="tabs">
+        <Link to="/StockItems" className="tab ">Todos os itens</Link>
+        <Link to="/NewItem" className="tab active">Novo Item</Link>
+      </div>
+
+      <form>
+        {/* Trocar nome da classe depois */}
+        <div className="display-flex-row">
+          <div className="form-group">
+          <label htmlFor="itemName">Nome:</label>
+          <input type="text" id="itemName" /> 
+          </div>
+
+          <div className="form-group">
+          <label htmlFor="itemQtd">Quantidade</label>
+          <input type="number" id="itemQtd"/>
+          </div>
+
+          <div className="form-group">   
+          <label htmlFor="priceItem">Preço</label>
+          <input type="number" id="priceItem" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="category">Categoria</label>
+            <select name="category" id="category">
+            <option value="">Selecione uma opção</option>
+            <option value="OptionA">Option A</option>
+            <option value="OptionB">Option B</option>
+            </select> {/* Substituir as opçoes posteriormente */}  
+          </div>          
+        </div>
+        <div className="descriptionArea">
+          <label htmlFor="description">Descrição</label>
+          <textarea name="description" id="description">
+          </textarea>
+        </div>
+        <button type="submit">Salvar</button>
+      </form>
     </>
   )
 }
