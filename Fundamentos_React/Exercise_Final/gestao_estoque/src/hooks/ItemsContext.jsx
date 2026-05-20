60e8b3cc17af6e37600507f84bb5a07e9e2cf385
Henrique <henriquedias628@gmail.com>
2026-05-20 17:04:13 -0300

    feat: criação da função addItem

--- Fundamentos_React/Exercise_Final/gestao_estoque/src/hooks/ItemsContext.jsx
+++ Fundamentos_React/Exercise_Final/gestao_estoque/src/hooks/ItemsContext.jsx
@@ -6,16 +6,28 @@ export const StockContext = createContext();
 
 export function StockContextProvider({ children }) {
   const [items, setItems] = useState(() => {
-    const stockItems = localStorage.getItem("item-");
+    const stockItems = localStorage.getItem("react-stock-items");
     /* Fazer teste pra ver se aparece o db  */
     // if (!stockItems) return [];
     if (!stockItems) return defaultItems;
     return JSON.parse(stockItems);
   });
+
+  function addItem(item) {
+    const id = "item-" + Math.floor(Math.random() * 1000000);
+    const newItem = {
+      /* Utilização do spread operator para receber as propriedades do objeto, name,description... Dica da IA pra ficar mais limpo (Tinha pensando em fazer name: name 
+      e assim por diante...) */
+      ...item,
+      id: id,
+    };
+
+    setItems((currentState) => {
+      const newState = [...currentState, newItem];
+      localStorage.setItem("react-stock-items", JSON.stringify(newState));
+      return newState;
+    });
+  }
 }
 
-return (
-  <>
-    <h1>Teste</h1>
-  </>
-);
+return <StockContext.Provider value={{ items, addItem }}>{children}</StockContext.Provider>;
